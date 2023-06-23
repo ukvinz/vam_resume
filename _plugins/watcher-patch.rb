42bfccd9dabb8efd743ef39be5e81644ceae4069
FerneyChen <ferneychen@gmail.com>
2023-06-23 19:51:35 +0800

    test

---
+++ _plugins/watcher-patch.rb
@@ -0,0 +1,13 @@
+require 'jekyll-watch'
+
+module Jekyll
+    module Watcher
+        extend self
+
+        alias_method :original_listen_ignore_paths, :listen_ignore_paths
+
+        def listen_ignore_paths(options)
+            original_listen_ignore_paths(options) + [%r!.*\.TMP!i]
+        end
+    end
+end
